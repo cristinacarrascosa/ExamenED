e3f493f2ef3113b1d3c4f49b09eff1410e139750
cristinacarrascosa <cctausias@gmail.com>
2021-12-01 18:11:39 +0100

    Nuevo proyecto descargado de GitHub despues del examen

---
+++ ExamenED/src/Reto2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author examened
+ */
+public class Reto2 {
+
+    public static void main(String[] args) {
+
+        int n = 0;
+
+        while (n < 1000) {
+
+            n = n + 1;
+            System.out.println(n);
+
+        }
+    }
+
+}
